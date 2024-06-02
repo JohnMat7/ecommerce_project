d9065c4aff0b2e6a4bbb64a9c0ca1cc4b77912cc
johnmat7 <joffyjoffy8@gmail.com>
2024-06-02 14:21:56 +0530

    updation

--- Services/User_managment/app/auth.py
+++ Services/User_managment/app/auth.py
@@ -2,14 +2,17 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 
-from . import schemas, models, crud
+from . import schemas, crud
 
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -33,4 +36,23 @@ def authenticate_user(db: Session, user_name: str, password: str):
         return False
     if not verify_password(password, user.hashed_password):
         return False
+    return user
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_name: str = payload.get("sub")
+        if user_name is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(user_name=user_name)
+    except JWTError:
+        raise credentials_exception
+    user = crud.get_user_by_user_name(db, user_name=token_data.user_name)
+    if user is None:
+        raise credentials_exception
     return user
--- Services/User_managment/app/database.py
+++ Services/User_managment/app/database.py
@@ -1,14 +1,19 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
 
-load_dotenv()
+SQLALCHEMY_DATABASE_URL="postgresql://john:johnmatjoffy7@localhost:5432/ecom_users"
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
